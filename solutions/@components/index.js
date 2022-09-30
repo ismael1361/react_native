ee0f406ab6a2f5d3b26ad74366398bce48bf0b62
Ismael Souza Silva <ismael1361@gmail.com>
2022-09-30 01:12:37 -0300

    202209300112

--- solutions/@components/index.js
+++
@@ -1,4 +0,0 @@
-import EffectView from "@components/EffectView"
-import Icon from "@components/Icon"
-
-export {EffectView, Icon}
--- solutions/@utils/index.js
+++
@@ -1,4 +0,0 @@
-import cssGradientParser from "@utils/cssGradientParser.js"
-import cssParser from "@utils/cssParser.js"
-
-export {cssGradientParser, cssParser}
